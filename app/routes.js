a86c2ff83150c8e0f587a09123f2c837e408e566
Bertrandmc <bemacode@gmail.com>
2021-02-25 17:05:40 +0000

    Implement lists POC v1

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,4 @@
+const querystring = require('querystring');
 const express = require('express')
 const router = express.Router()
 const _ = require('lodash')
@@ -6,17 +7,20 @@ const practiceAreasList = require('./data/practiceAreas.json')
 const thailandLawyers = require('./data/thailand-lawyers.json')
 const thailandMedicalFacilities = require('./data/thailand-medical-facilities.json')
 
-console.log(practiceAreasList)
-
 const fakeDB = {
   thailand: {
     lawyers: thailandLawyers,
     'medical-facilities': thailandMedicalFacilities
   },
-  spain: {}
+  spain: {},
+  query: function (params) {
+    const { country, serviceType } = params
+    return _.get(this, `${country.toLowerCase()}.${serviceType.toLowerCase()}`, [])
+  }
 }
 
 const DEFAULT_VIEW_PROPS = {
+  _,
   serviceName: 'Service finder',
   countriesList,
   practiceAreasList,
@@ -24,14 +28,87 @@ const DEFAULT_VIEW_PROPS = {
   thailandMedicalFacilities
 }
 
-router.get('/service-finder-v1/:serviceName?', function (req, res, next) {
-  res.render('service-finder-v1', { ...DEFAULT_VIEW_PROPS })
-})
+const v1Route = '/service-finder-v1/:find?'
+function v1RouteHandler (req, res) {
+  const { find } = req.params
+
+  const params = {
+    ...req.query,
+    ...req.body
+  }
+
+  const {
+    serviceType,
+    country,
+    region,
+    practiceArea,
+    legalAid
+  } = params
+
+  const isSearchingForLawyers = serviceType === 'lawyers'
+
+  const queryString = Object.keys(params)
+    .map(key => `${key}=${params[key]}`)
+    .join('&')
+
+  const viewProps = {
+    ...params,
+    ...DEFAULT_VIEW_PROPS,
+    responses: { ...req.query }
+  }
+
+  if (!find) {
+    return res.render('start-page', {
+      ...viewProps,
+      nextRoute: '/service-finder-v1/find',
+      previousRoute: '/service-finder-v1/'
+    })
+  }
+
+  let questionToRender
+  let searchResults = []
+
+  if (!serviceType) {
+    questionToRender = 'question-service-type.html'
+  } else if (!country) {
+    questionToRender = 'question-country.html'
+  } else if (!region) {
+    questionToRender = 'question-region.html'
+  } else if (!region) {
+    questionToRender = 'question-region.html'
+  } else if (!practiceArea && isSearchingForLawyers) {
+    questionToRender = 'question-practice-area.html'
+  } else if (!legalAid && isSearchingForLawyers) {
+    questionToRender = 'question-legal-aid.html'
+  } else if (req.method === 'POST') {
+    return res.redirect(`/service-finder-v1/find?${queryString}`)
+  } else {
+    searchResults = fakeDB.query({ country, serviceType })
+  }
+
+  return res.render('v1-service-finder', {
+    ...viewProps,
+    previousRoute: '/service-finder-v1/',
+    nextRoute: `/service-finder-v1/find?${queryString}`,
+    questionToRender,
+    params,
+    searchResults,
+    removeQueryParameter: (route, parameterName) => {
+      const [path, query] = route.split(/\?/)
+      const params = querystring.parse(query)
+      delete params[parameterName]
+      return `${path}?${querystring.stringify(params)}`
+    }
+  })
+}
+
+router.get(v1Route, v1RouteHandler)
+router.post(v1Route, v1RouteHandler)
 
 // V2 APP
 const v2Route =
   '/service-finder-v2/:find?/:serviceType?/:country?/:region?/:practiceArea?/:legalAid?'
-function v2RouteHandler (req, res, next) {
+function v2RouteHandler (req, res) {
   const viewProps = {
     ...DEFAULT_VIEW_PROPS,
     ...req.params
@@ -42,7 +119,7 @@ function v2RouteHandler (req, res, next) {
   const isSearchingForLawyers = serviceType === 'lawyers'
 
   if (!find) {
-    return res.render('v2-start', {
+    return res.render('start-page', {
       ...viewProps,
       nextRoute: '/service-finder-v2/find',
       previousRoute: '/service-finder-v2/'
@@ -50,7 +127,7 @@ function v2RouteHandler (req, res, next) {
   }
 
   if (!serviceType && !req.body.serviceType) {
-    return res.render('v2-question-service-type', {
+    return res.render('v2-page-question-service-type', {
       ...viewProps,
       nextRoute: '/service-finder-v2/find',
       previousRoute: '/service-finder-v2/'
@@ -60,7 +137,7 @@ function v2RouteHandler (req, res, next) {
   }
 
   if (!country && !req.body.country) {
-    return res.render('v2-question-country', {
+    return res.render('v2-page-question-country', {
       ...viewProps,
       nextRoute: `/service-finder-v2/find/${serviceType}`,
       previousRoute: '/service-finder-v2/find'
@@ -72,7 +149,7 @@ function v2RouteHandler (req, res, next) {
   }
 
   if (!region && !req.body.region) {
-    return res.render('v2-question-region', {
+    return res.render('v2-page-question-region', {
       ...viewProps,
       nextRoute: `/service-finder-v2/find/${serviceType}/${country}`,
       previousRoute: `/service-finder-v2/find/${serviceType}`
@@ -84,7 +161,7 @@ function v2RouteHandler (req, res, next) {
   }
 
   if (!practiceArea && isSearchingForLawyers && !req.body.practiceArea) {
-    return res.render('v2-question-practice-area', {
+    return res.render('v2-page-question-practice-area', {
       ...viewProps,
       nextRoute: `/service-finder-v2/find/${serviceType}/${country}/${region}`.toLowerCase(),
       previousRoute: `/service-finder-v2/find/${serviceType}/${country}`
@@ -96,7 +173,7 @@ function v2RouteHandler (req, res, next) {
   }
 
   if (!legalAid && isSearchingForLawyers && !req.body.legalAid) {
-    return res.render('v2-question-legal-aid', {
+    return res.render('v2-page-question-legal-aid', {
       ...viewProps,
       nextRoute: `/service-finder-v2/find/${serviceType}/${country}/${region}/${practiceArea}`.toLowerCase(),
       previousRoute: `/service-finder-v2/find/${serviceType}/${country}/${region}`.toLowerCase()
@@ -107,13 +184,12 @@ function v2RouteHandler (req, res, next) {
     )
   }
 
-  const listToRender = _.get(fakeDB, `${country.toLowerCase()}.${serviceType.toLowerCase()}`, [])
+  const searchResults = fakeDB.query({ country, serviceType })
 
-  return res.render('v2-list', {
+  return res.render('v2-page-results-list', {
     ...viewProps,
-    listToRender,
+    searchResults,
     isSearchingForLawyers,
-    _,
     previousRoute: `/service-finder-v2/find/${serviceType}/${country}/${region}/${practiceArea}/`
   })
 }
